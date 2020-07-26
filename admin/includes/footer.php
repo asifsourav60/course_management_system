f5057573e26d289236f0e6d1e855d781a34d9fa9
Asifuzzaman Sourav <55782988+asifsourav60@users.noreply.github.com>
2020-07-26 19:01:49 +0600

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer style="background-color:#B9BFBB">
+        <div class="container" >
+            <div class="row">
+                <div class="col-md-12">
+                    Bangladesh Army university  of Engineering & Technology <br> <a href="http://www.bauet.ac.bd/" target="_blank">www.bauet.ac.bd</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ admin/includes/header.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero" style="background-color:#B9BFBB;height:166px">
+        <div class="container">
+             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                 
+				<div class="header_n">
+				    <p style="color:#FDFEFE;font-size:30px;line-height:26px;margin-left:190px;margin-top:35px">Bangladesh Army university  of Engineering & Technology</p>
+				</div>
+				
+				<div class ="header_logo">
+				   <img style = "height:144px;width:168px;margin-top:-60px;"src = "assets/img/log.png"   class= "img-responsive"/>
+				</div>
+                  
+
+             
+
+             
+            </div>
+        </div>
+	</div>
